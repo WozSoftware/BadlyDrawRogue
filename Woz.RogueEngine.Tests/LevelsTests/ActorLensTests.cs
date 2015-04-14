0caeb9e229883cc1a7249e92873e311d8b92714c
Woz <warrenleggatt@gmail.com>
2015-04-14 20:04:06 +1200

    Actor tests tweaks and lenses tests complete

---
+++ Woz.RogueEngine.Tests/LevelsTests/ActorLensTests.cs
@@ -0,0 +1,87 @@
+﻿#region License
+// Copyright � Woz.Software 2015
+// [https://github.com/WozSoftware/BadlyDrawRogue]
+//
+// This file is part of Woz.RoqueEngine.
+//
+// Woz.RoqueEngine is free software: you can redistribute it 
+// and/or modify it under the terms of the GNU General Public 
+// License as published by the Free Software Foundation, either 
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System.Collections.Immutable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Woz.Lenses;
+using Woz.RogueEngine.Levels;
+
+namespace Woz.RogueEngine.Tests.LevelsTests
+{
+    using IStatisticsStore = IImmutableDictionary<StatisticTypes, int>;
+    using IThingStore = IImmutableDictionary<long, Thing>;
+
+    [TestClass]
+    public class ActorLensTests
+    {
+        [TestMethod]
+        public void Id()
+        {
+            var actor = ActorTests.Actor.Set(ActorLens.Id, 2);
+
+            Assert.AreEqual(2, actor.Get(ActorLens.Id));
+        }
+
+        [TestMethod]
+        public void ActorType()
+        {
+            var type = ActorTypes.Monster;
+            var actor = ActorTests.Actor.Set(ActorLens.ActorType, type);
+
+            Assert.AreEqual(type, actor.Get(ActorLens.ActorType));
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            var actor = ActorTests.Actor.Set(ActorLens.Name, "A");
+
+            Assert.AreEqual("A", actor.Get(ActorLens.Name));
+        }
+
+        [TestMethod]
+        public void Statistics()
+        {
+            var stats = new Mock<IStatisticsStore>().Object; 
+            var actor = ActorTests.Actor.Set(ActorLens.Statistics, stats);
+
+            Assert.AreEqual(stats, actor.Get(ActorLens.Statistics));
+        }
+
+        [TestMethod]
+        public void HitPoints()
+        {
+            var hp = Levels.HitPoints.Create(1, 1);
+            var actor = ActorTests.Actor.Set(ActorLens.HitPoints, hp);
+
+            Assert.AreEqual(hp, actor.Get(ActorLens.HitPoints));
+        }
+
+        [TestMethod]
+        public void Things()
+        {
+            var things = new Mock<IThingStore>().Object;
+            var actor = ActorTests.Actor.Set(ActorLens.Things, things);
+
+            Assert.AreEqual(things, actor.Get(ActorLens.Things));
+        }
+    }
+}
--- Woz.RogueEngine.Tests/LevelsTests/ActorTests.cs
+++ Woz.RogueEngine.Tests/LevelsTests/ActorTests.cs
@@ -25,100 +25,91 @@ using Woz.RogueEngine.Levels;
 
 namespace Woz.RogueEngine.Tests.LevelsTests
 {
+    using IStatisticsStore = IImmutableDictionary<StatisticTypes, int>;
+    using IThingStore = IImmutableDictionary<long, Thing>;
+
     [TestClass]
     public class ActorTests
     {
-        private const int Id = 1;
-        private const ActorTypes ActorType = ActorTypes.Monster;
-        private const string Name = "Name";
-        private readonly IImmutableDictionary<StatisticTypes, int> _stats = 
-            new Mock<IImmutableDictionary<StatisticTypes, int>>().Object;
-        private readonly HitPoints _hp = HitPoints.Create(1, 1);
-        private readonly IImmutableDictionary<long, Thing> _things = 
-            new Mock<IImmutableDictionary<long, Thing>>().Object;
+        public const int Id = 1;
+        public const ActorTypes ActorType = ActorTypes.Monster;
+        public const string Name = "Name";
+        public static readonly IStatisticsStore Stats = new Mock<IStatisticsStore>().Object;
+        public static readonly HitPoints Hp = HitPoints.Create(1, 1);
+        public static readonly IThingStore Things = new Mock<IThingStore>().Object;
+
+        public static readonly Actor Actor =
+            Actor.Create(Id, ActorType, Name, Stats, Hp, Things);
 
         [TestMethod]
         public void Create()
         {
-            var actor = Actor.Create(Id, ActorType, Name, _stats, _hp, _things);
-
-            Assert.AreEqual(Id, actor.Id);
-            Assert.AreEqual(ActorType, actor.ActorType);
-            Assert.AreEqual(Name, actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreEqual(Id, Actor.Id);
+            Assert.AreEqual(ActorType, Actor.ActorType);
+            Assert.AreEqual(Name, Actor.Name);
+            Assert.AreSame(Stats, Actor.Statistics);
+            Assert.AreSame(Hp, Actor.HitPoints);
+            Assert.AreSame(Things, Actor.Things);
         }
 
         [TestMethod]
         public void WithNoValues()
         {
-            var actor = Actor.Create(Id, ActorType, Name, _stats, _hp, _things);
-                
-            Assert.AreSame(actor, actor.With());
+            Assert.AreSame(Actor, Actor.With());
         }
 
         [TestMethod]
         public void WithId()
         {
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(id: 2);
+            var actor = Actor.With(id: 2);
 
             Assert.AreEqual(2, actor.Id);
             Assert.AreEqual(ActorType, actor.ActorType);
             Assert.AreEqual(Name, actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreSame(Stats, actor.Statistics);
+            Assert.AreSame(Hp, actor.HitPoints);
+            Assert.AreSame(Things, actor.Things);
         }
 
         [TestMethod]
         public void WithType()
         {
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(actorType: ActorTypes.Player);
+            var actor = Actor.With(actorType: ActorTypes.Player);
 
             Assert.AreEqual(Id, actor.Id);
             Assert.AreEqual(ActorTypes.Player, actor.ActorType);
             Assert.AreEqual(Name, actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreSame(Stats, actor.Statistics);
+            Assert.AreSame(Hp, actor.HitPoints);
+            Assert.AreSame(Things, actor.Things);
         }
 
         [TestMethod]
         public void WithName()
         {
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(name: "A");
+            var actor = Actor.With(name: "A");
 
             Assert.AreEqual(Id, actor.Id);
             Assert.AreEqual(ActorType, actor.ActorType);
             Assert.AreEqual("A", actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreSame(Stats, actor.Statistics);
+            Assert.AreSame(Hp, actor.HitPoints);
+            Assert.AreSame(Things, actor.Things);
         }
 
         [TestMethod]
         public void WithStats()
         {
-            var newStats = 
-                new Mock<IImmutableDictionary<StatisticTypes, int>>().Object;
+            var newStats = new Mock<IStatisticsStore>().Object;
 
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(statistics: newStats);
+            var actor = Actor.With(statistics: newStats);
 
             Assert.AreEqual(Id, actor.Id);
             Assert.AreEqual(ActorType, actor.ActorType);
             Assert.AreEqual(Name, actor.Name);
             Assert.AreSame(newStats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreSame(Hp, actor.HitPoints);
+            Assert.AreSame(Things, actor.Things);
         }
 
         [TestMethod]
@@ -126,33 +117,28 @@ namespace Woz.RogueEngine.Tests.LevelsTests
         {
             var newHp = HitPoints.Create(2, 2);
 
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(hitPoints: newHp);
+            var actor = Actor.With(hitPoints: newHp);
 
             Assert.AreEqual(Id, actor.Id);
             Assert.AreEqual(ActorType, actor.ActorType);
             Assert.AreEqual(Name, actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
+            Assert.AreSame(Stats, actor.Statistics);
             Assert.AreSame(newHp, actor.HitPoints);
-            Assert.AreSame(_things, actor.Things);
+            Assert.AreSame(Things, actor.Things);
         }
 
         [TestMethod]
         public void WithThings()
         {
-            var newThings =
-                new Mock<IImmutableDictionary<long, Thing>>().Object;
+            var newThings = new Mock<IThingStore>().Object;
 
-            var actor = Actor
-                .Create(Id, ActorType, Name, _stats, _hp, _things)
-                .With(things: newThings);
+            var actor = Actor.With(things: newThings);
 
             Assert.AreEqual(Id, actor.Id);
             Assert.AreEqual(ActorType, actor.ActorType);
             Assert.AreEqual(Name, actor.Name);
-            Assert.AreSame(_stats, actor.Statistics);
-            Assert.AreSame(_hp, actor.HitPoints);
+            Assert.AreSame(Stats, actor.Statistics);
+            Assert.AreSame(Hp, actor.HitPoints);
             Assert.AreSame(newThings, actor.Things);
         }
     }
