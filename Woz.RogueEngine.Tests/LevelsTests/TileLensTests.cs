1783c94e40f1a7b8fbd8ecf6cc3af617a82e1165
Woz <warrenleggatt@gmail.com>
2015-04-16 21:51:06 +1200

    Levels unit tests complete

---
+++ Woz.RogueEngine.Tests/LevelsTests/TileLensTests.cs
@@ -0,0 +1,63 @@
+#region License
+// Copyright (C) Woz.Software 2015
+// [https://github.com/WozSoftware/BadlyDrawRogue]
+//
+// This file is part of Woz.RoqueEngine.
+//
+// Woz.RoqueEngine is free software: you can redistribute it 
+// and/or modify it under the terms of the GNU General Public 
+// License as published by the Free Software Foundation, either 
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+using System.Collections.Immutable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Woz.Lenses.Tests;
+using Woz.Monads.MaybeMonad;
+using Woz.RogueEngine.Levels;
+
+namespace Woz.RogueEngine.Tests.LevelsTests
+{
+    using IThingStore = IImmutableDictionary<long, Thing>;
+
+    [TestClass]
+    public class TileLensTests : BaseLensTests
+    {
+        [TestMethod]
+        public void TileType()
+        {
+            TestLensWithAreEqual(
+                TileTests.Tile, TileLens.TileType, TileTypes.Floor);
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            TestLensWithAreEqual(TileTests.Tile, TileLens.Name, "A");
+        }
+
+        [TestMethod]
+        public void Actor()
+        {
+            TestLensWithAreSame(
+                TileTests.Tile, TileLens.Actor, 
+                new Mock<IMaybe<Actor>>().Object);
+        }
+
+        [TestMethod]
+        public void Things()
+        {
+            TestLensWithAreSame(
+                TileTests.Tile, TileLens.Things,
+                new Mock<IThingStore>().Object);
+        }
+    }
+}
